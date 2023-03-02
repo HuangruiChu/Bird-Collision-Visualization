d1395c5fc86f644306da0bfbbd9562d681493f04
储黄瑞 <65276824+HuangruiChu@users.noreply.github.com>
2023-03-02 16:34:27 -0500

    Update index.js

--- resources/Figure3/index.js
+++ resources/Figure3/index.js
@@ -1,1 +1,1 @@
-export {default} from "./86ddbc29bd33f9d6@353.js";
+export {default} from "https://raw.githubusercontent.com/HuangruiChu/Bird-Collision-Visualization/main/resources/Figure3/86ddbc29bd33f9d6@353.js";
